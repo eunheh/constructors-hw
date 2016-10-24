c4a4b275824117eea81964841655157eccae7c81
Grace Lee <eunheh@gmail.com>
2016-10-24 16:48:16 -0400

    completed normal

--- src/js/constructors/dog.js
+++ src/js/constructors/dog.js
@@ -1,4 +1,14 @@
-function Dog () {
-}
+function Dog (options) {
+  let opts = options || {};
+  this.status = opts.status = 'normal';
+  this.color = opts.color = 'black';
+
+  if (opts.hungry === false){
+      this.hungry = false;
+    } else {
+      this.hungry = true;
+    };
+
+};
 
 export { Dog };
--- src/js/constructors/person.js
+++ src/js/constructors/person.js
@@ -1,4 +1,23 @@
-function Person () {
+function Person (options) {
+  let opts = options || {};
+
+  if (opts.cool === true){
+      this.cool = true;
+    } else {
+      this.cool = false;
+    };
+
 };
+
+Person.prototype.pet = function (dog) {
+   dog.status = 'happy';
+}
+
+Person.prototype.feed = function (dog) {
+   dog.hungry = false;
+}
+
+Person.prototype.owner = {};
+
 
 export { Person };
